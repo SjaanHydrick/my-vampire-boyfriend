3d005d0087db6dbf41caff6ca9b33f872aef59f2
Evan Finkelstein <evanfinkelstein91@gmail.com>
2020-10-19 14:51:14 -0700

    worked on render function

--- utils/render-screen.js
+++ utils/render-screen.js
@@ -4,9 +4,9 @@ const screen = document.querySelector('#screen');
 const internalDialogueBox = document.createElement('p');
 const dialogueBox = document.createElement('p')
 const background = document.createElement('img');
-background.classList.add(background);
-background.src = `../assets/${cafeteria.image}`;
-
+background.classList.add('background');
+background.src = `../assets/${ cafeteria.image }`;
+// const section = document.createElement('section')
 
 
 internalDialogueBox.classList.add('hidden');
@@ -16,6 +16,8 @@ dialogueBox.classList.add('hidden');
 
 
 
+
+// screen.appendChild(section);
 screen.appendChild(background);
-screen.appendChild(internalDialogueBox);
-screen.appendChild(dialogueBox)
+// screen.appendChild(internalDialogueBox);
+// screen.appendChild(dialogueBox);
