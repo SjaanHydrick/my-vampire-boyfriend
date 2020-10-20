fa646bda7383c753db93fd1e369d802307679a03
Evan Finkelstein <evanfinkelstein91@gmail.com>
2020-10-20 13:48:44 -0700

    added restart button

--- utils/render-result-text.js
+++ utils/render-result-text.js
@@ -12,9 +12,28 @@ export function renderResultText(resultText) {
 
     if (player.vampire === 'dead' && player.tofu === true) {
 
-        resultText.textContent = `Your vampire BF dies of garlic, you made it through ${player.chapters} dates with your vampire boyfriend.` ;
+        resultText.textContent = `Your vampire BF dies of garlic.` ;
 
     }
 
 
 }
+
+
+
+export function renderDatesText(datesText) {
+    const player = getFromLocalStorage(storageKey);
+
+    if (player.chapters === 0) {
+
+        datesText.textContent = ` You didn't make it through a single date with your vampire Boyfriend. You absolute Adonnis.` ;
+
+    }
+
+
+}
+
+
+
+
+
--- utils/results.js
+++ utils/results.js
@@ -1,12 +1,19 @@
-import { renderResultText } from './render-result-text.js';
+import { renderResultText, renderDatesText} from './render-result-text.js';
 import { renderVent } from './render-vent.js';
 const screen = document.querySelector('#screenframe');
-
+const button = document.querySelector('#dial-button');
 const resultText = document.createElement('p');
+const numberOfDates = document.createElement('p');
 renderVent();
 renderResultText(resultText);
-
+renderDatesText(numberOfDates);
 screen.appendChild(resultText);
+screen.appendChild(numberOfDates);
+
+button.addEventListener('click', () => {
+    localStorage.clear();
+    window.location.href = '../index.html';
+});
 
 
 
