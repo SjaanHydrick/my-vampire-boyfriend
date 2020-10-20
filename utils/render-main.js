2b1b3a6a7c2efa96b99c3386631a8a301ebdec6e
Paul Stevens <twinkisan@hotmail.com>
2020-10-20 13:06:28 -0700

    fixes bugs in render-beat.js

--- utils/render-main.js
+++ utils/render-main.js
@@ -1,6 +1,5 @@
 import { renderScene } from './render-scene.js';
 import { renderVent } from './render-vent.js';
-import { S1B1 } from '../data/scene-one.js';
 import { I1 } from '../data/intro.js';
 
 // runs when screen first starts.  Renders Scene 1 Beat 1, and renders the 'vent' section
