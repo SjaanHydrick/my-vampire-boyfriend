bd2823da6abbe2be9b5f37ac67e60f77260a15d0
Sydney James <sydneyjames7@gmail.com>
2020-10-21 13:25:14 -0700

    scene five complete

--- data/scene-five.js
+++ data/scene-five.js
@@ -11,6 +11,7 @@ export const S5B1 = {
     leftChar: null,
     rightResize: true,
     rightChar: 'Vampire_BF.png',
+    buttonChoice: 'nav',
     dialogueBox: `Best night EVER! My boyfriend let me follow him back to his place. Wow it's so cavernous and dark, where should I go first?`,
     choices: [{
         id: 'library',
@@ -34,7 +35,7 @@ export const S5B1 = {
             resultText: `Hey...there's a coffin just my size in here! Your boyfriend kills you and buries you in an unmarked grave.` 
         },
         responseFunction: function() {
-            gameOverYouDied();
+            updateBeat(S5B4);
         },
     }]
 };
@@ -51,18 +52,14 @@ const S5B2 = {
             id: 'peruse',
             response: `Peruse the shelves`,
             responseFunction: function() {
-                updateBeat(S5B4);
+                updateBeat(S5B5);
             }
         },
         {
             id: 'ew',
             response: `Ew books...I'd rather get cozy`,
-            result: {
-                dead: 'player',
-                resultText: 'Your boyfriend also wants to get cozy...he sinks his fangs in you and kills you.'
-            },
             responseFunction: function() {
-                gameOverYouDied();
+                updateBeat(S5B6);
             }
         }
     ]
@@ -79,22 +76,15 @@ const S5B3 = {
         {
             id: 'yes-please',
             response: 'Yes puh-lease!',
-            result: {
-                dead: 'vampire',
-                resultText: `A vampire hunter bursts through a window and stabs your boyfriend straight throught the heart! :'(`
-            },
             responseFunction: function() {
-                gameOverVampireDied();
+                updateBeat(S5B7);
             }
         },
         {
             id: 'no-please',
             response: 'Um no thanks',
-            result: {
-                resultText: 'Your vampire boyfriend surprisingly decides to reward your modest behavior by letting you live! Congrats!'
-            },
             responseFunction: function() {
-                youWin();
+                updateBeat(S5B8);
             }
 
         }
@@ -102,6 +92,18 @@ const S5B3 = {
 };
 
 const S5B4 = {
+    id: 'coffin',
+    leftChar: null,
+    rightResize: true,
+    rightChar: 'Vampire_BF.png',
+    buttonChoice: 'dial',
+    dialogueBox: `Hey...there's a coffin just my size in here! Your boyfriend kills you and buries you in an unmarked grave.`,
+    responseFunction: function() {
+        gameOverYouDied();
+    },
+};
+
+const S5B5 = {
     id: 'vampire-books',
     leftChar: null,
     rightResize: true,
@@ -112,25 +114,78 @@ const S5B4 = {
         {
             id: 'run',
             response: `RUN!!!!!`,
-            result: {
-                dead: 'player',
-                resultText: `You know his secret, you must die.`
-            },
             responseFunction: function() {
-                gameOverYouDied();
+                updateBeat(S5B9);
             }
         },
         {
             id: 'true-love',
             response: `Whatevs, you love him!`,
-            result: {
-                resultText: 'Your boyfriend loves you too and wants to be with you forever! He turns you into a vampire!!'
-            },
             responseFunction: function() {
-                isVampire();
-                youWin();
+                updateBeat(S5B10);
 
             }
         }
     ]
+};
+
+const S5B6 = {
+    id: 'also-cozy',
+    leftChar: null,
+    rightResize: true,
+    rightChar: 'Vampire_BF.png',
+    buttonChoice: 'dial',
+    dialogueBox: `Your boyfriend also wants to get cozy...he sinks his fangs in you and kills you.`,
+    responseFunction: function() {
+        gameOverYouDied();
+    },
+};
+
+const S5B7 = {
+    id: 'vampire-hunter',
+    leftChar: null,
+    rightResize: true,
+    rightChar: 'Vampire_BF.png',
+    buttonChoice: 'dial',
+    dialogueBox: `A vampire hunter bursts through a window and stabs your boyfriend straight throught the heart! :(`,
+    responseFunction: function() {
+        gameOverVampireDied();
+    },
+};
+
+const S5B8 = {
+    id: 'you-win',
+    leftChar: null,
+    rightResize: true,
+    rightChar: 'Vampire_BF.png',
+    buttonChoice: 'dial',
+    dialogueBox: `Your vampire boyfriend surprisingly decides to reward your modest behavior by letting you live! Congrats!`,
+    responseFunction: function() {
+        youWin();
+    },
+};
+
+const S5B9 = {
+    id: 'die',
+    leftChar: null,
+    rightResize: true,
+    rightChar: 'Vampire_BF.png',
+    buttonChoice: 'dial',
+    dialogueBox: `You know his secret, there is nowhere to run, so you must die.`,
+    responseFunction: function() {
+        gameOverYouDied();
+    },
+};
+
+const S5B10 = {
+    id: 'turn-vampire',
+    leftChar: null,
+    rightResize: true,
+    rightChar: 'Vampire_BF.png',
+    buttonChoice: 'dial',
+    dialogueBox: `Your boyfriend loves you too and wants to be with you forever! He turns you into a vampire!!`,
+    responseFunction: function() {
+        isVampire();
+        youWin();
+    },
 };
