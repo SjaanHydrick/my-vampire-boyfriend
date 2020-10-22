aa4262d1917e59fa1ca1761cb2da89d35fb52635
Paul Stevens <twinkisan@hotmail.com>
2020-10-22 13:19:10 -0700

    clearing dead code

--- utils/button-tutorial.js
+++ utils/button-tutorial.js
@@ -1,21 +1,24 @@
 
-export function buttonTutorial(){
+export function buttonTutorial() {
 
     const buttons = document.getElementsByClassName('small-buttons');
-    // const responses = document.getElementById('response1');
-        for (let i = 0; i < buttons.length; i++){
-            buttons[i].classList.add(`glow${i+1}`);
-            // responses[i].classList.add(`glow`);
-        }
+    const responses = document.getElementsByClassName('response');
 
-//     buttons[0].classList.add('glow1');
-//     responses[0].classList.add('glow1')
+    console.log(responses)
 
-//     buttons[1].classList.add('glow2');
-//     responses[1].classList.add('glow1')
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.add(`glow${i + 1}`);
+        responses[i].classList.add(`glow`);
+    }
 
-//     buttons[2].classList.add('glow3');
-//     responses[2].classList.add('glow3')
+    //     buttons[0].classList.add('glow1');
+    //     responses[0].classList.add('glow1')
 
-// }
+    //     buttons[1].classList.add('glow2');
+    //     responses[1].classList.add('glow1')
+
+    //     buttons[2].classList.add('glow3');
+    //     responses[2].classList.add('glow3')
+
+    // }
 }
