7780f2f8fdba434f6ce513b02929220df24913d9
Paul Stevens <twinkisan@hotmail.com>
2020-10-22 10:31:52 -0700

    fixes bug in volume slider

--- utils/render-main.js
+++ utils/render-main.js
@@ -3,7 +3,6 @@ import { I1 } from '../data/intro.js';
 import { createGameAudio } from './game-audio.js';
 import { setVolumeLevel } from './manage-local-storage.js';
 
-setVolumeLevel(0.5);
 createGameAudio();
 
 renderScene(I1);
